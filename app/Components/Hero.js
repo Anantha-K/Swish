aef699a64e3890c1c782a431cb1ab10756c3dd08
Anantha Krishnan <jananthak@gmail.com>
2024-03-17 23:42:33 +0530

    nav

---
+++ app/Components/Hero.js
@@ -0,0 +1,32 @@
+'use client'
+import React, { useRef, useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
+
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+import './Styles/Hero.css'
+
+// import required modules
+import { Pagination } from 'swiper/modules';
+
+export default function Hero() {
+  return (
+    <>
+      <Swiper pagination={true} modules={[Pagination, Autoplay]} autoplay={{ delay: 3000, disableOnInteraction: false }} loop className='mySwiper'>
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+    </>
+  );
+}
