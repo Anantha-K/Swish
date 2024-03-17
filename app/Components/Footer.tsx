f130b0d8ce50a986b2d5cd67df6b6b7012b70814
Anantha Krishnan <jananthak@gmail.com>
2024-03-18 00:23:38 +0530

    fix

---
+++ app/Components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+<>
+<footer className='bg-black w-full h-24 items-center flex text-white justify-center text-3xl bottom-0'>
+    Swish
+
+</footer>
+</>  )
+}
+
+export default Footer
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
 import Image from "next/image";
-import Nav from './Components/Nav.js'
 import Hero from './Components/Hero.js'
+
 export default function Home() {
   return (
   <>
-  <Nav/>
   <Hero/>
   </>
   );
