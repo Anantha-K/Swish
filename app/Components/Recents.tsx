2d5188fb716e75af6b3deb97dce15e3cd6be87da
Anantha Krishnan <89961607+Anantha-K@users.noreply.github.com>
2024-04-02 23:25:50 +0530

    Update Recents.tsx

--- app/Components/Recents.tsx
+++ app/Components/Recents.tsx
@@ -5,19 +5,6 @@ import React from "react"
 import fetch from "../fetch/fetch"
 
 
-// const products = [
-//     {
-//       id: 1,
-//       name: 'Basic Tee',
-//       href: '#',
-//       imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
-//       imageAlt: "Front of men's Basic Tee in black.",
-//       price: '$35',
-//       color: 'Black',
-//     },
-//     // More products...
-//   ]
-  
 export default async function Recents() {
     const products = await fetch.getLatest();
     return (
